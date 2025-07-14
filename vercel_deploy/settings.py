3bcc1bdf8fa5cbd8f0053b0c894e4862990dbf44
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-07-14 10:57:16 +0700

    disable bb

--- vercel_deploy/settings.py
+++ vercel_deploy/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'vercel_deploy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
