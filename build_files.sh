8d3ac4c01cdf6d0766fede04b85b4a468fa5301a
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-07-22 00:23:40 +0700

    C

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
+# python3.12 -m pip install -r requirements.txt
+# python3.12 manage.py collectstatic --noinput --clear
+
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
