7f8fca263613fc690e41eb5ee0107edf73adec41
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-07-22 21:31:54 +0700

    C

--- webpage/views.py
+++ webpage/views.py
@@ -1,25 +1,25 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 # from django.http import HttpResponse
 # from . import views
 
 
 
 # Create your views here.
-# def home(request):
-#     return render(request, 'home.html')
+def home(request):
+    return render(request, 'home.html')
 
-# def about(request):
-#     return render(request, 'about.html')
+def about(request):
+    return render(request, 'about.html')
 
-# def contact(request):
-#     return render(request, 'contact.html')
+def contact(request):
+    return render(request, 'contact.html')
 
 # urls.py
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),  # 👈 ชื่อของ path นี้คือ 'home'
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-]
+# urlpatterns = [
+#     path('', views.home, name='home'),  # 👈 ชื่อของ path นี้คือ 'home'
+#     path('about/', views.about, name='about'),
+#     path('contact/', views.contact, name='contact'),
+# ]
