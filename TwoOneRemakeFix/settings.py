8699ed8e38f01b7280ffbbe11ba69e974875834c
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-07-22 22:30:20 +0700

    C

--- TwoOneRemakeFix/settings.py
+++ TwoOneRemakeFix/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'TwoOneRemakeFix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        # 'DIRS': ['templates'],
+         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
